f1c633139fbdff3bfdd981ce29aeadcc66492e94
Anders Giovanni Møller <67187134+AGMoller@users.noreply.github.com>
2023-02-23 16:23:07 +0100

    Update logging in training

--- src/setfit/modeling.py
+++ src/setfit/modeling.py
@@ -345,7 +345,7 @@ class SetFitModel(PyTorchModelHubMixin):
 
                 # Log the loss to wandb if initialized
                 if wandb.run is not None:
-                    wandb.log({"train_loss": epoch_loss}, step=epoch_idx)
+                    wandb.log({"train/train_loss": epoch_loss}, step=epoch_idx)
                 else:
                     print(f"Epoch {epoch_idx} loss: {epoch_loss}")
 
